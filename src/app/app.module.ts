812eed3515a3bd97c008515829b1abc940f8e15d
Supakarn Jamsai <supakarn.j@b-connex.net>
2023-11-17 11:45:01 +0700

    create component customer-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './core/component/login-page/login-page.component';
 import { FormsModule } from '@angular/forms';
+import { CustomerPageComponent } from './customer/component/customer-page/customer-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    CustomerPageComponent
   ],
   imports: [
     FormsModule,
