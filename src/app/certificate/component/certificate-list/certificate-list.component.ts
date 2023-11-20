7e5664dd8d584d70f038281a381b8594c9f7d14b
weeraphon.s <weeraphon.s@b-connex.net>
2023-11-20 10:38:44 +0700

    add select box

--- src/app/certificate/component/certificate-list/certificate-list.component.ts
+++ src/app/certificate/component/certificate-list/certificate-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { certificateListViewModel, searchParamCertificateList } from '../../models/certificate-list.model';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-certificate-list',
@@ -7,7 +8,41 @@ import { certificateListViewModel, searchParamCertificateList } from '../../mode
   styleUrls: ['./certificate-list.component.css']
 })
 export class CertificateListComponent {
+  @ViewChild('importTemplate') importTemplate!: TemplateRef<any>;
   public searchParam: searchParamCertificateList = new searchParamCertificateList();
+  public modalRef!: BsModalRef;
+  public fileToUpload: any;
+  public materialDataSource = [
+    {
+      text: '2CTFB : เหล็กแผ่นดำ ตัดซอยตามขนาด',
+      value: '2CTFB'
+    }
+  ];
+  public gradeDataSource = [
+    {
+      text: 'SS400',
+      value: 'SS400'
+    }
+  ];
+  public millDataSource = [
+    {
+      text: 'SSI',
+      value: 'SSI'
+    },
+    {
+      text: 'GJ',
+      value: 'GJ'
+    },
+    {
+      text: 'GJS',
+      value: 'GJS'
+    },
+    {
+      text: 'SYS',
+      value: 'SYS'
+    },
+
+  ];
   public list: certificateListViewModel[] = [
     {
       certNo: "42523050483",
@@ -19,21 +54,21 @@ export class CertificateListComponent {
       modifiedTime: new Date(),
       dataSource: [
         {
-        material : '2CTFB',
-        materialDesc : 'เหล็กแผ่นดำ ตัดซอยตามขนาด',
-        grade : 'SS400',
-        heatNo : 'DB10177',
-        quantity : 8,
-        unit: 'PC'
+          material: '2CTFB',
+          materialDesc: 'เหล็กแผ่นดำ ตัดซอยตามขนาด',
+          grade: 'SS400',
+          heatNo: 'DB10177',
+          quantity: 8,
+          unit: 'PC'
         },
         {
-          material : '2CTFB020-0060-1180',
-          materialDesc : 'เหล็กแผ่นดำ 2.00 x 60 x 1180 mm SS400',
-          grade : 'SS400',
-          heatNo : 'DB10178',
-          quantity : 2,
+          material: '2CTFB020-0060-1180',
+          materialDesc: 'เหล็กแผ่นดำ 2.00 x 60 x 1180 mm SS400',
+          grade: 'SS400',
+          heatNo: 'DB10178',
+          quantity: 2,
           unit: 'PC'
-          }
+        }
       ]
     },
     {
@@ -46,21 +81,21 @@ export class CertificateListComponent {
       modifiedTime: new Date(),
       dataSource: [
         {
-        material : '2CTFB',
-        materialDesc : 'เหล็กแผ่นดำ ตัดซอยตามขนาด',
-        grade : 'SS400',
-        heatNo : 'DB10177',
-        quantity : 8,
-        unit: 'PC'
+          material: '2CTFB',
+          materialDesc: 'เหล็กแผ่นดำ ตัดซอยตามขนาด',
+          grade: 'SS400',
+          heatNo: 'DB10177',
+          quantity: 8,
+          unit: 'PC'
         },
         {
-          material : '2CTFB020-0060-1180',
-          materialDesc : 'เหล็กแผ่นดำ 2.00 x 60 x 1180 mm SS400',
-          grade : 'SS400',
-          heatNo : 'DB10178',
-          quantity : 12,
+          material: '2CTFB020-0060-1180',
+          materialDesc: 'เหล็กแผ่นดำ 2.00 x 60 x 1180 mm SS400',
+          grade: 'SS400',
+          heatNo: 'DB10178',
+          quantity: 12,
           unit: 'PC'
-          }
+        }
       ]
     },
     {
@@ -73,27 +108,37 @@ export class CertificateListComponent {
       modifiedTime: new Date(),
       dataSource: [
         {
-          material : '2CTFB020-0060-1180',
-          materialDesc : 'เหล็กแผ่นดำ 2.00 x 60 x 1180 mm SS400',
-          grade : 'SS400',
-          heatNo : 'DB10178',
-          quantity : 2,
+          material: '2CTFB020-0060-1180',
+          materialDesc: 'เหล็กแผ่นดำ 2.00 x 60 x 1180 mm SS400',
+          grade: 'SS400',
+          heatNo: 'DB10178',
+          quantity: 2,
           unit: 'PC'
-          }
+        }
       ]
     }
   ];
 
-  constructor(){
+  constructor(private modalService: BsModalService) {
 
   }
 
-  public onClickSearch():void {
+  public handleFileInput($event: any): void {
+    this.fileToUpload = $event?.target?.files[0];
+  }
+
+  public onClickSearch(): void {
 
   }
 
-  public onClickClear():void {
+  public onClickClear(): void {
 
+  }
+
+  public onClickUpload(): void {
+    this.modalRef = this.modalService.show(this.importTemplate, {
+      class: 'modal-lg'
+    })
   }
 
 }
