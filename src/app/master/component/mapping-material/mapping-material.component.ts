3f032bd01e7f55f12c1851b124c6eff1d9f5be84
Supakarn Jamsai <supakarn.j@b-connex.net>
2023-11-20 11:13:11 +0700

    update modal mapping mat

--- src/app/master/component/mapping-material/mapping-material.component.ts
+++ src/app/master/component/mapping-material/mapping-material.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { mappingMaterialViewModel } from '../../models/mapping-material.model';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'mapping-material',
   templateUrl: './mapping-material.component.html',
   styleUrls: ['./mapping-material.component.css']
 })
-export class MappingMaterialComponent {
+export class MappingMaterialComponent implements OnInit {
 
+  public modalRef!: BsModalRef;
+
+  public clickImport!: true;
+  public fileToUpload!: File;
+  public dataForUpload!: Array<any>;
+
+  @ViewChild('addMappingMaterial') public addMappingMaterial!: TemplateRef<any>;
+  @ViewChild('importMappingMaterial') public importMappingMaterial!: TemplateRef<any>;
+  @ViewChild('editMappingMaterial') public editMappingMaterial!: TemplateRef<any>;
+  
   public dataSource: mappingMaterialViewModel[] = [
     {
       index: 1,
@@ -33,21 +44,33 @@ export class MappingMaterialComponent {
 
   ];
 
+  constructor(private modalService: BsModalService) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+
   public onClickSearch(): void {
-    
+
   }
 
   public onClickClear(): void {
-    
+
   }
 
   // Add
   public onClickAdd(): void {
-    
+    this.modalRef = this.modalService.show(this.addMappingMaterial, {
+      class: 'modal-xl'
+    });
   }
 
   public onClickExitAddModal(): void {
-    
+    this.modalRef.hide();
   }
 
   public onClickConfirmAddMappingMaterial(): void {
@@ -55,51 +78,80 @@ export class MappingMaterialComponent {
   }
 
   public onClickExitAddMappingMaterial(): void {
-    
+    this.modalRef.hide();
   }
 
   // Import
   public onClickImport(): void {
+    this.modalRef = this.modalService.show(this.importMappingMaterial, {
+      class: 'modal-xl'
+    });
+  }
+
+  public handleFileInput($event: any): void {
+    this.fileToUpload = $event?.target?.files[0];
+  }
+
+  public uploadFile() : void {
+
+  }
+
+  public onClearFile(fileInput: any) : void {
     
   }
 
+
   public onClickExitImportModal(): void {
-    
+    this.modalRef.hide();
   }
-  
-  public downloadTemplate(): void {
-    
+
+  public onClickDownloadTemplate(): void {
+
   }
 
   public onClickConfirmImportMappingMaterial(): void {
-    
+
   }
 
   public onClickExitImportMappingMaterial(): void {
-    
+    this.modalRef.hide();
   }
+
+  //export
 
   public onClickExport(): void {
-    
+
   }
 
+  public materialCode?: string;
+  public materialDesc?: string;
+
   //Edit
-  public onClickEditMappingMaterial(): void {
+  public onClickEditMappingMaterial(template: TemplateRef<any>,cell: any): void {
+    this.materialCode = cell.data.materialCode;
+    this.materialDesc = cell.data.materialDesc;
     
+    this.modalRef = this.modalService.show(this.editMappingMaterial, {
+      class: 'modal-xl'
+    });
+  }
+
+  public onClickExitEditMappingMaterial(): void {
+    this.modalRef.hide();
   }
 
   public onClickSaveEditMappingMaterial(): void {
-    
+
   }
 
   public onClickCancelEditMappingMaterial(): void {
-    
+
   }
 
   //Delete
   public onClickDeleteMappingMaterial(): void {
-    
+
   }
 
-  
+
 }
