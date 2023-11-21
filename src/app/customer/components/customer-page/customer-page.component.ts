31a0ab25f1672ca3803eab1a2501a5670da9ba22
Supakarn Jamsai <supakarn.j@b-connex.net>
2023-11-21 16:17:11 +0700

    change date

--- src/app/customer/components/customer-page/customer-page.component.ts
+++ src/app/customer/components/customer-page/customer-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CustomerHistoryViewModel } from '../../models/customer-history.model';
+import { CustomerHistorySearchParam, CustomerHistoryViewModel } from '../../models/customer-history.model';
 
 @Component({
   selector: 'customer-page',
@@ -7,51 +7,76 @@ import { CustomerHistoryViewModel } from '../../models/customer-history.model';
   styleUrls: ['./customer-page.component.css']
 })
 export class CustomerPageComponent {
+
+  public param: CustomerHistorySearchParam = new CustomerHistorySearchParam();
   public dataSource: CustomerHistoryViewModel[] = [
     {
       doNo: "21567891",
-      date: new Date,
+      date: '',
       itemNo: 1,
       materialDesc: "ท่อดำ 3/4x 2.0mm x 6400mm",
       qty: 20
     },
     {
       doNo: "21567891",
-      date: new Date,
+      date: '',
       itemNo: 2,
       materialDesc: "เหล็กฉาก SS400 ความยาวใดๆ",
       qty: 10
     },
     {
       doNo: "21567892",
-      date: new Date,
+      date: '',
       itemNo: 1,
       materialDesc: "เหล็กลายพับตามแบบ",
       qty: 50
     },
     {
       doNo: "21567893",
-      date: new Date,
+      date: '',
       itemNo: 1,
       materialDesc: "ตัวซีมีขอบ ขนาดใดๆ",
       qty: 5
     },
     {
       doNo: "21567893",
-      date: new Date,
+      date: '',
       itemNo: 2,
       materialDesc: "เหล็กลายพับตามแบบ",
       qty: 10
     },
     {
       doNo: "21567893",
-      date: new Date,
+      date: '',
       itemNo: 3,
       materialDesc: "เหล็กฉาก SS400 ความยาวใดๆ",
       qty: 25
     }
 
   ];
+
+  public dateDataSource: any[] = [
+    {
+      text: 'ภายใน 90 วัน',
+      value: 90
+    },
+    {
+      text: 'ภายใน 60 วัน',
+      value: 60
+    },
+    {
+      text: 'ภายใน 30 วัน',
+      value: 30
+    },
+    {
+      text: 'ภายใน 7 วัน',
+      value: 7
+    },
+    {
+      text: 'ระบุเอง',
+      value: 0
+    }
+  ]
 
   public onClickDownload(): void {
     alert("Download Success!");
--- src/app/customer/models/customer-history.model.ts
+++ src/app/customer/models/customer-history.model.ts
@@ -1,7 +1,28 @@
 export class CustomerHistoryViewModel {
-    doNo?: string;
-    date?: Date;
-    itemNo?: number;
-    materialDesc?: string;
-    qty?: number;
+    doNo: string;
+    date: string;
+    itemNo: number;
+    materialDesc: string;
+    qty: number;
+
+    constructor() {
+        this.doNo = '';
+        this.date = (new Date()).toString();
+        this.itemNo = 0;
+        this.materialDesc = '';
+        this.qty = 0;
+    }
+
+}
+
+export class CustomerHistorySearchParam {
+    materialDesc: string;
+    dateFrom: number;
+    dateTo: number;
+    
+    constructor() {
+        this.materialDesc = '' ;
+        this.dateFrom = 90;
+        this.dateTo = 90;
+    }
 }
