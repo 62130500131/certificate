cc54bf1b198e0ab15badb0275f9df304c38b44bc
weeraphon.s <weeraphon.s@b-connex.net>
2023-11-22 19:07:59 +0700

    edit addDetail map certificate

--- src/app/modules/map-certificate/components/do-shipment-entry/do-shipment-entry.component.ts
+++ src/app/modules/map-certificate/components/do-shipment-entry/do-shipment-entry.component.ts
@@ -1,5 +1,5 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { DoShipmentEntryViewModel, SelectQuantity } from '../../models/do.model';
+import { DoShipmentDetail, DoShipmentEntryViewModel, SelectQuantity } from '../../models/do.model';
 import { Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
@@ -9,8 +9,9 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
   styleUrls: ['./do-shipment-entry.component.css']
 })
 export class DoShipmentEntryComponent {
-  @ViewChild('selectTemplate') selectTemplate! :TemplateRef<any>;
-  @ViewChild('addDetailTemplate') addDetailTemplate! :TemplateRef<any>;
+  @ViewChild('selectTemplate') selectTemplate!: TemplateRef<any>;
+  @ViewChild('addDetailTemplate') addDetailTemplate!: TemplateRef<any>;
+  public isType3: boolean = false;
   public modalRef!: BsModalRef;
   public list: DoShipmentEntryViewModel[] = [
     {
@@ -18,30 +19,34 @@ export class DoShipmentEntryComponent {
       dataSource: [
         {
           itemIndex: 1,
-          material: '2CTFB',
+          materialCode: '2CTFB',
+          materialDesc: 'เหล็กแผ่นดำ ตัดซอยตามขนาด',
           heatNo: 'DB1077',
-          quantity:  40, 
+          quantity: 40,
           status: 'Select'
         },
         {
           itemIndex: 2,
-          material: '2CTFB',
+          materialCode: '1AK106-025-00062',
+          materialDesc: 'เหล็กฉากSS400 25 x 25 x 3.00mm x 6.0M',
           heatNo: 'DB1078',
-          quantity:  20, 
+          quantity: 20,
           status: 'Edit'
         },
         {
           itemIndex: 3,
-          material: '2CTFB',
+          materialCode: '1BP0200-045-1430',
+          materialDesc: 'ท่อดำ 8" x 4.50mm x 6.0M',
           heatNo: 'DB1079',
-          quantity:  40, 
+          quantity: 40,
           status: 'Select'
         },
         {
           itemIndex: 4,
-          material: '2CTFB',
+          materialCode: '3UC00753223-0141',
+          materialDesc: 'เหล็กตามแบบ 75x32x32x2.30mmx6.0M',
           heatNo: 'DB1077',
-          quantity:  30, 
+          quantity: 30,
           status: 'Select'
         }
       ]
@@ -51,9 +56,10 @@ export class DoShipmentEntryComponent {
       dataSource: [
         {
           itemIndex: 1,
-          material: '2CTFB',
+          materialCode: '1AK106-030-00081',
+          materialDesc: 'เหล็กฉากSS400 30 x 30 x 3.00mm x 6.0M',
           heatNo: 'DB1077',
-          quantity:  22, 
+          quantity: 22,
           status: 'Select'
         }
       ]
@@ -63,9 +69,10 @@ export class DoShipmentEntryComponent {
       dataSource: [
         {
           itemIndex: 1,
-          material: '2CTFB',
+          materialCode: '2CTFB',
+          materialDesc: 'เหล็กแผ่นดำ ตัดซอยตามขนาด',
           heatNo: 'DB1079',
-          quantity:  20, 
+          quantity: 20,
           status: 'Select'
         }
       ]
@@ -73,67 +80,87 @@ export class DoShipmentEntryComponent {
 
   ]
 
+  public editDataSource: SelectQuantity[] = [
+
+  ]
+
   public selectDataSource: SelectQuantity[] = [
     {
-      isSelected : false,
-      itemIndex : 1,
-      material : '2CTFB',
-      materialDesc : 'เหล็กดำ ตัดซอยตามขนาด',
-      heatNo : 'DB1077',
-      mill : 'SYS',
-      remain : 20, 
-      quantity : 20 
+      isSelected: false,
+      itemIndex: 1,
+      material: '2CTFB',
+      materialDesc: 'เหล็กดำ ตัดซอยตามขนาด',
+      heatNo: 'DB1077',
+      mill: 'SYS',
+      remain: 20,
+      quantity: 20
     },
     {
-      isSelected : false,
-      itemIndex : 2,
-      material : '2CTFB',
-      materialDesc : 'เหล็กดำ ตัดซอยตามขนาด',
-      heatNo : 'DB1077',
-      mill : 'SYS',
-      remain : 20, 
-      quantity : 20 
+      isSelected: false,
+      itemIndex: 2,
+      material: '2CTFB',
+      materialDesc: 'เหล็กดำ ตัดซอยตามขนาด',
+      heatNo: 'DB1077',
+      mill: 'SYS',
+      remain: 20,
+      quantity: 20
     },
     {
-      isSelected : false,
-      itemIndex : 3,
-      material : '2CTFB',
-      materialDesc : 'เหล็กดำ ตัดซอยตามขนาด',
-      heatNo : 'DB1099',
-      mill : 'SYS',
-      remain : 20, 
-      quantity : 20 
+      isSelected: false,
+      itemIndex: 3,
+      material: '2CTFB',
+      materialDesc: 'เหล็กดำ ตัดซอยตามขนาด',
+      heatNo: 'DB1099',
+      mill: 'SYS',
+      remain: 20,
+      quantity: 20
     }];
 
   constructor(private router: Router,
-              private modalService: BsModalService){
+    private modalService: BsModalService) {
 
   }
 
-  public onClickExit():void{
+  public onClickExit(): void {
     this.router.navigate(['do-page'])
   }
 
-  public onClickSelect():void {
-    this.modalRef =  this.modalService.show(this.selectTemplate, {
-      class : 'modal-xl'
+  public onClickEdit(): void {
+    this.modalRef = this.modalService.show(this.selectTemplate, {
+      class: 'modal-xl'
+    })
+  }
+  
+  public onClickSelect(): void {
+    this.selectDataSource.forEach(x => x.isSelected = false)
+    this.modalRef = this.modalService.show(this.selectTemplate, {
+      class: 'modal-xl'
     })
   }
 
-  public onClickConfirmSelect():void{
+  public onClickConfirmSelect(): void {
+    this.list[0].dataSource[0].status = 'Edit';
     this.modalRef.hide();
   }
 
-  public onClickAddDetail():void {
+  public onClickAddDetail(cellData: DoShipmentDetail): void {
+    this.isType3 = false;
+    if (cellData.materialCode.substring(0, 1) == '3') {
+      this.isType3 = true;
+    }
     this.modalRef = this.modalService.show(this.addDetailTemplate, {
-      class: 'modal-lg'
+      class: 'modal-xl'
     })
   }
-  public onClickConfirmAddDetail(): void{
+  public onClickConfirmAddDetail(): void {
     this.modalRef.hide();
   }
 
-  public onClickSentLink():void {
+  public onCheckBoxChange(event: any, cellData: any) {
+    cellData.isSelected = !cellData.isSelected;
+  }
+
+  public onClickSentLink(): void {
     alert('Sent link success!')
   }
 }
