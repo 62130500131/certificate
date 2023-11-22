d78266e5009539babb0bbeefafff039273d11d6e
weeraphon.s <weeraphon.s@b-connex.net>
2023-11-22 14:04:41 +0700

    import excel customer contract

--- src/app/modules/master/master.module.ts
+++ src/app/modules/master/master.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import * as comps from './components';
 import { SharedModule } from '../shared/shared.module';
-import { CommonModule } from '@angular/common';
 @NgModule({
     declarations: [
         comps.MappingMaterialComponent,
