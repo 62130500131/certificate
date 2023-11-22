33df3f47a323cf07849299d57cb13e12e852704a
weeraphon.s <weeraphon.s@b-connex.net>
2023-11-22 15:14:24 +0700

    export customer contact

--- src/app/modules/quality-assurance/components/nac-certificate-entry/nac-certificate-entry.component.ts
+++ src/app/modules/quality-assurance/components/nac-certificate-entry/nac-certificate-entry.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxExtendedPdfViewerService, pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
-import { certificateEntryListViewModel } from 'src/app/modules/certificate/models/certificate-list.model';
 import { CertificateData } from '../../models/qa-status.model';
 
 @Component({
