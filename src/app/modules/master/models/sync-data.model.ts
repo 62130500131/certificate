53d7253da332175621afc6df9e2fb7b8b509c914
Supakarn Jamsai <supakarn.j@b-connex.net>
2023-11-27 18:53:59 +0700

    fix 31

--- src/app/modules/master/models/sync-data.model.ts
+++ src/app/modules/master/models/sync-data.model.ts
@@ -8,3 +8,29 @@ export class SyncDataViewModel {
         this.lastSyncTime = '';
     }
 }
+
+export class SyncLogsViewModel {
+    syncTime: string;
+    actionBy: string;
+    status: string;
+    constructor() {
+        this.syncTime = '';
+        this.actionBy = '';
+        this.status = '';
+    }
+}
+export class SyncLogsParam {
+    date: number;
+    dateFrom: string;
+    dateTo: string;
+    statusFailed: string;
+    statusSuccess: string;
+    
+    constructor() {
+        this.date = 0;
+        this.dateFrom = '';
+        this.dateTo = '';
+        this.statusFailed = 'statusFailed';
+        this.statusSuccess = 'statusSuccess';
+    }
+}
