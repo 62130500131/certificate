891eada9e06b2adb31fa79d8868ab0702d770ab7
Supakarn Jamsai <supakarn.j@b-connex.net>
2023-12-04 13:54:05 +0700

    เพิ่มคำอธิบาย หน้า Qa , Production

---
+++ src/app/modules/production/services/production.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { InformationViewModel } from '../models/production.model';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductionService {
+
+  private info: InformationViewModel[] = [
+    {
+      status: "Wait Sample",
+      explanation: ""
+    },
+    {
+      status: "Wait Film",
+      explanation: ""
+    },
+    {
+      status: "Sample Ready",
+      explanation: ""
+    },
+    {
+      status: "Film Ready",
+      explanation: ""
+    },
+    {
+      status: "Complete",
+      explanation: ""
+    },
+    {
+      status: "Cancel",
+      explanation: ""
+    },
+  ]
+
+  public getInformation(): Observable<InformationViewModel[]> {
+    return of(this.info);
+  }
+
+}
